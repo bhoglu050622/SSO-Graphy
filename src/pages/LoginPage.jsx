548656f5d1568e35341d7e4af257eff5aa778b6e
bhoglu050622 <amanbhogal.work@gmail.com>
2025-06-29 23:51:20 +0530

    Fix: Correct LoginModal import path

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSearchParams } from "react-router-dom";
-import LoginModal from "../components/LoginModal";
+import LoginModal from '../components/HomeComponents/LoginModal';
 
 const LoginPage = () => {
   const [searchParams] = useSearchParams();
